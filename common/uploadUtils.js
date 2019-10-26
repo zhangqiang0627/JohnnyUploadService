a51a54fab331200680b59ad301b9d4b28017642a
zhangqiang <johnny.q.zhang@outlook.com>
2019-10-26 13:02:38 +0800

    文件上传API开发完成

---
+++ common/uploadUtils.js
@@ -0,0 +1,56 @@
+let path = require('path');
+let fs = require("fs");
+let multer = require('multer');
+
+exports.createBankWorkerObject = function (folders) {
+  let rootDir = path.resolve(__dirname, '..');
+  for(let i = 0; i < folders.length; i++){
+    rootDir = path.join(rootDir, folders[i]);
+  }
+  makeDir(rootDir);
+
+  let storage = multer.diskStorage({
+    destination: function (req, file, cb){
+      let bankCode = req.query.bankCode;
+      let branchCode = req.query.branchCode;
+      let dirName = req.query.dirName;
+      let uploadDir = path.join(rootDir, bankCode, branchCode);
+      if(dirName !== undefined){
+        uploadDir = path.join(uploadDir, dirName);
+      }
+      makeDir(uploadDir);
+      cb(null, uploadDir);
+    },
+    filename: function (req, file, cb){
+      //设置文件的名字为其原本的名字，也可以添加其他字符，来区别相同文件，例如file.originalname+new Date().getTime();利用时间来区分
+      cb(null, file.originalname)
+    }
+  });
+
+  return multer({storage: storage});
+};
+
+function makeDir(dirpath) {
+  if (!fs.existsSync(dirpath)) {
+    let pathtmp;
+    dirpath.split("/").forEach(function(dirname) {
+      if (pathtmp) {
+        pathtmp = path.join(pathtmp, dirname);
+      }
+      else {
+        //如果在linux系统中，第一个dirname的值为空，所以赋值为"/"
+        if(dirname){
+          pathtmp = dirname;
+        }else{
+          pathtmp = "/";
+        }
+      }
+      if (!fs.existsSync(pathtmp)) {
+        if (!fs.mkdirSync(pathtmp)) {
+          return false;
+        }
+      }
+    });
+  }
+  return true;
+}
---
+++ routes/upload.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let uploadUtils = require('../common/uploadUtils');
+let router = express.Router();
+
+let bankWorkerObject = uploadUtils.createBankWorkerObject(['public','bankWorker']);
+const bankWorkerFileMaxCount = 10;
+
+router.post('/bankWorker',  bankWorkerObject.array('file', bankWorkerFileMaxCount), function(req,res,next){
+  let fileUrlList = [];
+  let host = req.headers.host;
+  let bankCode = req.query.bankCode;
+  let branchCode = req.query.branchCode;
+  let dirName = req.query.dirName;
+
+  let fileUrl = `http://${host}/bankWorker/${bankCode}/${branchCode}/`;
+
+  if(dirName !== undefined){
+    fileUrl = fileUrl.concat(`${dirName}/`);
+  }
+
+  req.files.forEach(function (file, index) {
+    fileUrlList.push(fileUrl.concat(file.originalname))
+  });
+
+  res.json({
+    err : false,
+    fileUrlList : fileUrlList
+  });
+  res.end();
+});
+
+module.exports = router;
